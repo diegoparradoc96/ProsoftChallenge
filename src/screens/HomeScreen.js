0bfb819b838786761575e323dab7642228ab5a2b
Diego Parrado <diegoparrado.cardozo@hotmail.com>
2023-01-17 18:53:51 -0500

    ...

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 import {
   Text,
-  Button,
+  TouchableOpacity,
   StyleSheet,
   TextInput,
   ToastAndroid,
 } from "react-native";
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useFormik } from "formik";
 import * as Yup from "yup";
@@ -20,57 +21,93 @@ export default function HomeScreen(props) {
     validationSchema: Yup.object(validationSchema()),
     validateOnChange: false,
     onSubmit: (formValues) => {
-      const { codigo, nombre } = formValues;
-      console.log("formValues: ", codigo, nombre);
+      const { cedula, nombres, apellidos, celular } = formValues;
+      console.log("formValues: ", cedula, nombres, apellidos, celular);
     },
   });
 
   return (
+    <KeyboardAwareScrollView>
     <SafeAreaView>
       <Text style={styles.title}>Formulario</Text>
+
+      <Text style={styles.label}>Cedula</Text>
       <TextInput
-        placeholder="Codigo"
+        placeholder="1121935723"
         style={styles.input}
         autoCapitalize="none"
-        value={formik.values.codigo}
-        onChangeText={(text) => formik.setFieldValue("codigo", text)}
+        value={formik.values.cedula}
+        onChangeText={(text) => formik.setFieldValue("cedula", text)}
       />
+
+      <Text style={styles.label}>Nombres</Text>
       <TextInput
-        placeholder="Nombre"
+        placeholder="Luis Enrique"
         style={styles.input}
         autoCapitalize="none"
-        value={formik.values.nombre}
-        onChangeText={(text) => formik.setFieldValue("nombre", text)}
+        value={formik.values.nombres}
+        onChangeText={(text) => formik.setFieldValue("nombres", text)}
       />
-      <Button
+
+      <Text style={styles.label}>Apellidos</Text>
+      <TextInput
+        placeholder="Petro Guarin"
+        style={styles.input}
+        autoCapitalize="none"
+        value={formik.values.apellidos}
+        onChangeText={(text) => formik.setFieldValue("apellidos", text)}
+      />
+
+      <Text style={styles.label}>Celular</Text>
+      <TextInput
+        placeholder="3208275850"
+        style={styles.input}
+        autoCapitalize="none"
+        value={formik.values.celular}
+        onChangeText={(text) => formik.setFieldValue("celular", text)}
+      />
+
+      <TouchableOpacity
         title="Guardar"
         onPress={formik.handleSubmit}
         style={styles.button}
-      />
+      >
+        <Text style={styles.textButton}>Guardar</Text>
+      </TouchableOpacity>
       {/* <Text style={styles.error}>{formik.errors.codigo}</Text> */}
       {/* <Text style={styles.error}>{formik.errors.nombre}</Text> */}
     </SafeAreaView>
+    </KeyboardAwareScrollView>
   );
 }
 
 function initialValues() {
   return {
-    codigo: "",
-    nombre: "",
+    cedula: "",
+    nombres: "",
+    apellidos: "",
   };
 }
 
 function validationSchema() {
-  const showToastCodigo = () => {
-    ToastAndroid.show("El codigo es obligatorio", ToastAndroid.SHORT);
+  const showToastCedula = () => {
+    ToastAndroid.show("Cedula requerida", ToastAndroid.SHORT);
   };
-  const showToastNombre = () => {
-    ToastAndroid.show("El nombre es obligatorio", ToastAndroid.SHORT);
+  const showToastNombres = () => {
+    ToastAndroid.show("Nombre requerido", ToastAndroid.SHORT);
+  };
+  const showToastApellidos = () => {
+    ToastAndroid.show("Apellidos requerido", ToastAndroid.SHORT);
+  };
+  const showToastCelular = () => {
+    ToastAndroid.show("Celular requerido", ToastAndroid.SHORT);
   };
 
   return {
-    codigo: Yup.string().required(() => showToastCodigo()),
-    nombre: Yup.string().required(() => showToastNombre()),
+    cedula: Yup.string().required(() => showToastCedula()),
+    nombres: Yup.string().required(() => showToastNombres()),
+    apellidos: Yup.string().required(() => showToastApellidos()),
+    celular: Yup.string().required(() => showToastCelular()),
   };
 }
 
@@ -82,6 +119,12 @@ const styles = StyleSheet.create({
     marginTop: 20,
     marginBottom: 15,
   },
+  label: {
+    marginLeft: 18,
+    marginBottom: -10,
+    fontWeight: "bold",
+    fontSize: 15,
+  },
   input: {
     height: 40,
     margin: 12,
@@ -90,7 +133,17 @@ const styles = StyleSheet.create({
     borderRadius: 10,
   },
   button: {
-    paddingTop: 50,
+    justifyContent: "center",
+    margin: 12,
+    height: 45,
+    borderRadius: 7,
+    backgroundColor: "#1b6dff",
+  },
+  textButton: {
+    color: "#fff",
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: 20,
   },
   error: {
     textAlign: "center",
